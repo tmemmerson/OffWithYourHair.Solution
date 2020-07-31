28d93dc85aeeddd632855ce15f9bed1fe2eae508
Tristan Emmerson <tristan@stickerslug.com>
2020-07-31 13:45:00 -0700

    add stylist controller create actionresult.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -20,5 +20,10 @@ namespace HairSalon.Controllers
       List<Stylist> model = _db.Stylists.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
   }
 }
