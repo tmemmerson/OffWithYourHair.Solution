475f641382fe2feda5b21648b1b7ae43908b7dc0
Tristan Emmerson <tristan@stickerslug.com>
2020-07-31 13:58:30 -0700

    add actionresult delete method to stylistscontroller.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -56,7 +56,13 @@ namespace HairSalon.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Delete (int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(Stylist => Stylist.StylistId == id);
+      return View(thisStylist);
+    }
 
     
+
   }
 }
