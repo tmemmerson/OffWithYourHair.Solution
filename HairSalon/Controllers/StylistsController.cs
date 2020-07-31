d3eb89a1d1774ffeac910d43ac7d35bd49b1569f
Tristan Emmerson <tristan@stickerslug.com>
2020-07-31 13:45:29 -0700

    add http post create method to controller.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -25,5 +25,14 @@ namespace HairSalon.Controllers
     {
       return View();
     }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
   }
 }
