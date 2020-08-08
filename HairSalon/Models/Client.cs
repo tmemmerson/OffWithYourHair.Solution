61e3686daa7e7c5c0bb096e3a757db179757e841
Tristan Emmerson <tristan@stickerslug.com>
2020-08-08 15:07:29 -0700

    update model

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,7 +5,7 @@ namespace HairSalon.Models
     public int ClientId { get; set; }
     public string Name { get; set; }
     public string Note { get; set; }
-    public int StylistId { get; set; } = 0;
+    public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
 }
